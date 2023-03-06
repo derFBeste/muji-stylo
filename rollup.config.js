7c43663fd0d9a4c31801f082590829da397d14be
derFBeste <fred@besteman.net>
2023-03-06 14:54:12 -0500

    chore: updates ci workflow

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,3 @@
-// see output.format: https://rollupjs.org/guide/en/#core-functionality
-// import svg from 'rollup-plugin-svg'
-// import { terser } from 'rollup-plugin-terser'
-// import typescript from "@rollup/plugin-typescript";
-// import pkg from "./package.json";
-
-// https://github.com/rollup/plugins/tree/master/packages/typescript
-
-// export default {
-//   input: "src/index.ts",
-//   external: Object.keys(pkg.peerDependencies || {}),
-//   output: {
-//     dir: "dist",
-//     format: "esm",
-//   },
-//   plugins: [typescript()],
-// };
-
 import withSolid from "rollup-preset-solid";
 import css from "rollup-plugin-import-css";
 
@@ -26,6 +8,5 @@ export default withSolid({
 
 // TODO: add server support
 // TODO: svg support
-// TODO: use tsup?
 // TODO: tree shake
 // TODO: source maps
